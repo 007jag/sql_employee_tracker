d1b8507bb1d2e582ac11ab243e6a2f665a45a017
jag cheema <jagmitsingh2003@gmail.com>
2023-03-11 22:35:22 -0500

    updated read.me

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const promptUser = () => {
         .prompt({
             type: 'list',
             name: 'begin choices',
-            message: 'What would you like to do? (Select on of the following)',
+            message: 'What would you like to do? (Select one of the following)',
             choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Update Employee Role', 'View Departments', 'Add Department', 'View Roles', 'Add Role','View total budget', 'I am finished']
         })
         .then((data) => {
